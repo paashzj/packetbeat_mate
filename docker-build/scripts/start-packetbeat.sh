070dfb99bdc40a39b3cb9f7da2cda64a060b9cf7
shoothzj <shoothzj@gmail.com>
2021-09-08 21:45:11 +0800

    optimize stdout code

--- docker-build/scripts/start-packetbeat.sh
+++ docker-build/scripts/start-packetbeat.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup $PACKETBEAT_HOME/heartbeat -e -c $PACKETBEAT_HOME/heartbeat.yml >$PACKETBEAT_HOME/packetbeat.stdout.log 2>$PACKETBEAT_HOME/packetbeat.stderr.log &
+nohup $PACKETBEAT_HOME/packetbeat -e -c $PACKETBEAT_HOME/packetbeat.yml >>$PACKETBEAT_HOME/packetbeat.stdout.log 2>>$PACKETBEAT_HOME/packetbeat.stderr.log &
