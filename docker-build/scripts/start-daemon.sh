3db124ad441c94b01ce2ec62c8063bd3b404645b
lovehzj <1922919664@qq.com>
2022-01-09 14:51:34 +0800

    fix mate log path

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+mkdir $PACKETBEAT_HOME/logs
+nohup $PACKETBEAT_HOME/mate/packetbeat_mate >>$PACKETBEAT_HOME/logs/packetbeat_mate.stdout.log 2>>$PACKETBEAT_HOME/packetbeat_mate.stderr.log
 
-nohup $PACKETBEAT_HOME/mate/packetbeat_mate >$PACKETBEAT_HOME/packetbeat_mate.stdout.log 2>$PACKETBEAT_HOME/packetbeat_mate.stderr.log
