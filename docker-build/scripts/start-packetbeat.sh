1a1cdb461e185a38b01ee28bd5a3aa4846d07b08
lovehzj <1922919664@qq.com>
2022-01-09 19:24:51 +0800

    fix kernel log path

--- docker-build/scripts/start-packetbeat.sh
+++ docker-build/scripts/start-packetbeat.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-nohup $PACKETBEAT_HOME/packetbeat -e -c $PACKETBEAT_HOME/packetbeat.yml >>$PACKETBEAT_HOME/packetbeat.stdout.log 2>>$PACKETBEAT_HOME/packetbeat.stderr.log &
+mkdir $PACKETBEAT_HOME/logs
+nohup $PACKETBEAT_HOME/packetbeat -e -c $PACKETBEAT_HOME/packetbeat.yml >>$PACKETBEAT_HOME/logs/packetbeat.stdout.log 2>>$PACKETBEAT_HOME/logs/packetbeat.stderr.log &
+
